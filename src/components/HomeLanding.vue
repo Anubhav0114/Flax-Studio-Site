af157c417bd9849152093a1972efc25173657c8d
nitesh-dev <niteshdev547@gmail.com>
2023-04-03 11:32:58 +0530

    Initial commit

---
+++ src/components/HomeLanding.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="landing-top">
+    <div class="header">
+
+    </div>
+   
+  </div>
+</template>
+
+<style scoped>
+
+.landing-top{
+    
+    width: 100%;
+    height: 400px;
+    background: linear-gradient(90deg, #6F40BD 0%, #D33396 100%);
+}
+
+.header{
+    width: 100%;
+}
+
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HomeLanding from '../components/HomeLanding.vue';
+
+
+</script>
+
+<template>
+  <div>
+    <HomeLanding/>
+
+  </div>
+</template>
+
+<style scoped></style>
