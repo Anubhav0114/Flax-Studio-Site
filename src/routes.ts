d9ab22ae25d6dc515943dd57a2fb7bc9409a7495
nitesh-dev <niteshdev547@gmail.com>
2023-04-04 19:48:07 +0530

    fix router 2

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import HomeVue from "./pages/Home.vue";
 import NotFoundVue from "./pages/NotFound.vue";
 
 export const routes = [
-    {path: '/:catchAll(.*)',component: NotFoundVue},
+    { path: '/:pathMatch(.*)*', name: 'NotFound',component: NotFoundVue},
     { path: "/", name: "root", component: HomeVue },
     { path: "/draw-on", name: "Draw On", component: DrawOnVue },
     // {path:"/SignUp", name:"SignUp", component:SignUpVue}
