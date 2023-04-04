9ad8aab7abbf58229cb3beb811dea374468a3f92
nitesh-dev <niteshdev547@gmail.com>
2023-04-05 02:10:27 +0530

    privacy added

---
+++ src/components/PrivacyLanding.vue
@@ -0,0 +1,167 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="landing-top">
+        <div class="header-parent">
+            <div class="header">
+                <h2><a href="#">Flax Studio</a></h2>
+                <div></div>
+            </div>
+        </div>
+
+        <div class="content-parent">
+            <div class="content">
+                <div class="detail">
+                    <h1>Draw On</h1>
+                    <p>A simple & easy drawing app to show your creativity.</p>
+                    <p>
+                        Draw On is a versatile drawing app that lets you
+                        create stunning illustrations and designs with ease.
+                        With its intuitive interface and powerful tools,
+                        you can draw rectangles, lines, circles, curves,
+                        and more with just a few taps and swipes.
+                    </p>
+
+                    <a href="#">Get from Playstore</a> <a href="#">View App</a>
+                </div>
+                <div></div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+.landing-top {
+    width: 100%;
+    overflow-y: auto;
+    background: linear-gradient(90deg, #6F40BD 0%, #D33396 100%);
+}
+
+
+.header-parent {
+    background-color: rgba(255, 255, 255, 0.13);
+}
+
+.header {
+    max-width: var(--max-page-width);
+    margin: auto;
+    height: 60px;
+    display: grid;
+    grid-template-columns: 1fr max-content;
+    align-items: center;
+}
+
+.header h2 {
+    font-size: 24px;
+    margin-left: var(--page-margin);
+    font-weight: 600;
+}
+
+.header a {
+    color: white;
+    text-decoration: none;
+}
+
+.header div {
+    margin-right: 12px;
+}
+
+.header div a {
+    color: rgb(255, 255, 255);
+    text-decoration: none;
+    margin: 0 12px;
+    font-size: medium;
+}
+
+.header div a:hover {
+    text-decoration: underline;
+}
+
+.content-parent {
+    max-width: var(--max-page-width);
+    margin: auto;
+}
+
+.content {
+    margin: 40px var(--page-margin);
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 50px;
+    color: white;
+}
+
+.content h1 {
+    font-weight: 600;
+    margin-top: 40px;
+    font-size: 40px;
+}
+
+.content p {
+    margin: 24px 0;
+}
+
+
+.content a {
+    display: inline-block;
+    margin-top: 30px;
+    text-decoration: none;
+    padding: 8px 24px;
+    border-radius: var(--default-border-radius);
+    font-size: 18px;
+    background-color: transparent;
+    border: 2px solid White;
+    color: white;
+    transition: background-color 200ms;
+    font-weight: 600;
+}
+
+
+.content a:hover {
+    background-color: white;
+    border: 2px solid White;
+    color: var(--color-primary);
+}
+
+.content img {
+    display: block;
+    margin: auto;
+    margin-top: 30px;
+    height: 300px;
+
+}
+
+
+
+@media only screen and (max-width: 700px) {
+    .content {
+        grid-template-columns: 100%;
+        text-align: center;
+    }
+
+    .header h2 {
+        font-size: 22px;
+        margin-left: var(--page-margin);
+        font-weight: 600;
+    }
+
+    .content h1 {
+        font-size: 36px;
+    }
+
+    .content img {
+        margin-top: 30px;
+        width: 100%;
+        height: auto;
+    }
+
+}
+
+@media only screen and (max-width: 400px) {
+    .hide-responsive {
+        display: none;
+    }
+}
+</style>
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
-import DrawOnVue from "./pages/Draw-On.vue";
+import DrawOnVue from "./pages/DrawOn.vue";
+import DrawOnPrivacyVue from "./pages/DrawOnPrivacy.vue";
 import HomeVue from "./pages/Home.vue";
 import NotFoundVue from "./pages/NotFound.vue";
 
@@ -6,5 +7,6 @@ export const routes = [
     { path: '/:pathMatch(.*)*', name: 'NotFound',component: NotFoundVue},
     { path: "/", name: "root", component: HomeVue },
     { path: "/draw-on", name: "Draw On", component: DrawOnVue },
+    { path: "/draw-on/privacy", name: "Draw On Privacy", component: DrawOnPrivacyVue },
     // {path:"/SignUp", name:"SignUp", component:SignUpVue}
 ]
