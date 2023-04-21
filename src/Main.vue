22ce43451c5ababd4d3f1839349c46a1d40a4b2d
nitesh-dev <niteshdev547@gmail.com>
2023-04-21 18:14:02 +0530

    copyright added

--- src/Main.vue
+++ src/Main.vue
@@ -1,10 +1,12 @@
 <script setup lang='ts'>
+import Copyright from './components/Copyright.vue';
 
 </script>
 <template>
     <!-- <router-link to="/">Home</router-link>
     <router-link to="/draw-on">About</router-link> -->
     <RouterView></RouterView>
+    <Copyright/>
     
 </template>
 <style scoped></style>
---
+++ src/components/Copyright.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+let year = new Date().getFullYear()
+</script>
+<template>
+    <div id="copyright-container">
+        <p>Copyright © {{ year }}. All Rights Reserved</p>
+    </div>
+</template>
+<style scoped>
+#copyright-container {
+    border-top: 1px solid rgb(235, 235, 235);
+}
+
+#copyright-container p {
+    margin-top: 32px;
+    margin-bottom: 32px;
+    font-size: 14px;
+    color: var(--color-on-secondary);
+    text-align: center;
+}
+</style>
